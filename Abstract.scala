44c2fe90aa8fbb554529f7bbc4b9aab01a75c270
long <longer.ztc@gmail.com>
2016-06-27 15:11:12 +0800

    Array and import

---
+++ Abstract.scala
@@ -0,0 +1,11 @@
+object HelloWorld {
+	def main(args: Array[String]): Unit = {
+		println("Hello World")
+	}
+}
+
+object MyMath {
+	def myProduct(num1: Int, num2: Int): Int = {
+		return num1 * num2
+	}
+}
--- Test.scala
+++ Test.scala
@@ -1,9 +1,32 @@
 object HelloWorld {
-	def main(): Unit = {
+	def main(args: Array[String]): Unit = {
 		println("Hello World")
 	}
 }
 
-helloWorld.main()
+object MyMath extends App {
+	def myProduct(num1: Int, num2: Int): Int = {
+		return num1 * num2
+	}
 
-helloWorld.main()
+	def myProduct2(nums: Array[Int]): Int = {
+		var res = 1
+
+		nums.foreach(res *= _)
+
+		return res
+	}
+
+	println(myProduct(10, 20))
+}
+
+object Main {
+
+	import MyMath._
+
+	def main(args: Array[String]) {
+		var nums = Array(1, 2, 3, 4)
+
+		println(myProduct2(nums))
+	}
+}
